3ffed0ece5327a129fd1d4e5b99e02e69c85f2aa
llutti <llutti@outlook.com>
2020-07-08 16:01:54 -0300

    Melhoria as definições do SEO

--- components/SEO/index.js
+++ components/SEO/index.js
@@ -13,6 +13,22 @@ const SEO = ({ title }) =>
       <title>{title} - {SEOConfig.title}</title>
       <meta name="description" content={SEOConfig.description}></meta>
       <meta name="author" content={SEOConfig.author} />
+      <meta name="image" content={SEOConfig.image} />
+
+      // OpenGraph (Facebook)
+      {SEOConfig.type && <meta property="og:type" content={SEOConfig.type} />}
+      {SEOConfig.url && <meta property="og:url" content={SEOConfig.url} />}
+      {SEOConfig.title && <meta property="og:title" content={SEOConfig.title} />}
+      {SEOConfig.description && <meta property="og:description" content={SEOConfig.description} />}
+      {SEOConfig.image && <meta property="og:image" content={SEOConfig.image} />}
+
+      // Twitter
+      <meta name="twitter:card" content="summary_large_image" />
+      {SEOConfig.twitterUsername && <meta name="twitter:creator" content={SEOConfig.twitterUsername} />}
+      {SEOConfig.title && <meta name="twitter:title" content={SEOConfig.title} />}
+      {SEOConfig.description && <meta name="twitter:description" content={SEOConfig.description} />}
+      {SEOConfig.image && <meta name="twitter:image" content={SEOConfig.image} />}
+
     </Head>
   )
 };
--- config/SEOConfig.js
+++ config/SEOConfig.js
@@ -1,12 +1,9 @@
 export const SEOConfig = {
   title: 'PalpiteBox',
   description: 'Uma caixa de sugestões na qual os clientes podem deixar opiniões e sugestões sobre os estabelecimentos comerciais e, em troca, receber cupons de desconto e vantagens.',
-  author:'Luciano Cargnelutti',
-  openGraph: {
-      type: 'website',
-      url: 'https://palpitebox-blue.vercel.app',
-      title: 'PalpiteBox',
-      description: 'Uma caixa de sugestões na qual os clientes podem deixar opiniões e sugestões sobre os estabelecimentos comerciais e, em troca, receber cupons de desconto e vantagens.',
-      image: '/print-palpitebox.png',
-  }
+  author: 'Luciano Cargnelutti',
+  image: '/print-palpitebox.png',
+  url: 'https://palpitebox-blue.vercel.app',
+  type: 'website',
+  twitterUsername: null,
 };
